69e39f2d63919c3c2de480a1bbc8f030e92dbdfd
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 14:49:01 +0900

    正常系のテストを追加する

--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -1,28 +1,12 @@
-describe Sample do
-  let(:sample){ Sample.new }
+describe ClosedRange do
+  let(:closed_range){ ClosedRange.new }
 
-  describe "#say" do
-    describe "Using expect to" do
-      let(:greeting){ "Hello" }
-
-      it "should say 'Hello TDD BootCamp!'" do
-        expect(sample.say(greeting)).to eq "Hello TDD BootCamp!"
-      end
-    end
-
-    describe "Using subject it should " do
-      subject { sample.say(greeting) }
-
-      let(:greeting){ "Yeah!" }
-      it { should eq "Yeah! TDD BootCamp!" }
-    end
-
-    describe "Using power assert " do
-      let(:greeting){ "Wow!" }
-
-      it do
-        is_asserted_by{ sample.say(greeting) == "Wow! TDD BootCamp!" }
-      end
+  describe '#display_range' do
+    example '整数の下端と上端（下端 < 上端）を引数に渡せば、下端と上端を値にもつArrayを返す' do
+      # example "should say 'Hello TDD BootCamp!'" do
+      #   expect(sample.say(greeting)).to eq "Hello TDD BootCamp!"
+      # end
+      expect(closed_range.display_range(1,3)).to eq [1, 3]
     end
   end
 end
