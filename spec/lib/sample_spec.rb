4fc18e2abfc59dae6748e35096a684deab3f91ad
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 14:49:39 +0900

    メソッド名のリファクタリング

--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -1,12 +1,9 @@
 describe ClosedRange do
   let(:closed_range){ ClosedRange.new }
 
-  describe '#display_range' do
+  describe '#display_range_array' do
     example '整数の下端と上端（下端 < 上端）を引数に渡せば、下端と上端を値にもつArrayを返す' do
-      # example "should say 'Hello TDD BootCamp!'" do
-      #   expect(sample.say(greeting)).to eq "Hello TDD BootCamp!"
-      # end
-      expect(closed_range.display_range(1,3)).to eq [1, 3]
+      expect(closed_range.display_range_array(1,3)).to eq [1, 3]
     end
   end
 end
