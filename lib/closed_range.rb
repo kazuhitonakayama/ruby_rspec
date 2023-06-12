52cc4ee8096506ee9fb093a65238083973c14c5f
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 15:40:11 +0900

    :sparkles: Integerではない値を渡した時にはNotIntegerValueIsProvided例外を返す実装

--- lib/closed_range.rb
+++ lib/closed_range.rb
@@ -6,9 +6,9 @@ end
 
 class ClosedRange
   def display_range_array(start_point, end_point)
+    return NotIntegerValueIsProvided unless start_point.is_a?(Integer) && end_point.is_a?(Integer)
     return [start_point] if start_point == end_point
     return StandardError if start_point > end_point
-    return FloatNotAllowedError unless start_point.is_a?(Integer) && end_point.is_a?(Integer)
 
     [start_point, end_point]
   end
