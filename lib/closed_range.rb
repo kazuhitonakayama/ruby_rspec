547e3f8b1e73700ef4be01909fb1f7c97327c051
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 15:04:36 +0900

    :white_check_mark: 終点よりも開始点が大きい時にはStandardErrorを返すテストの追加

---
+++ lib/closed_range.rb
@@ -0,0 +1,5 @@
+class ClosedRange
+  def display_range_array(start_point, end_point)
+    [start_point, end_point]
+  end
+end
--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -2,8 +2,68 @@ describe ClosedRange do
   let(:closed_range){ ClosedRange.new }
 
   describe '#display_range_array' do
-    example '整数の下端と上端（下端 < 上端）を引数に渡せば、下端と上端を値にもつArrayを返す' do
-      expect(closed_range.display_range_array(1,3)).to eq [1, 3]
+    describe '整数の下端と上端（下端 < 上端）を引数に渡せば、下端と上端を値にもつArrayを返す' do
+      example '下端として1, 上端として3を渡せば[1, 3]を返す' do
+        expect(closed_range.display_range_array(1, 3)).to eq [1, 3]
+      end
+    end
+
+    describe '上端よりも下端が大きい' do
+      example '下端として3, 上端として1' do
+        expect(closed_range.display_range_array(3, 1)).to eq StandardError
+      end
+    end
+
+    describe '下端と上端が等しい' do
+      example '' do
+      end
+    end
+
+    describe '下端が負である' do
+      example '' do
+      end
+    end
+
+    describe '小数を引数に渡す' do
+      example '' do
+      end
+    end
+
+    describe '数字の文字列を引数に渡す' do
+      example '' do
+      end
+    end
+
+    describe '数字ではない文字列を引数に渡す' do
+      example '' do
+      end
+    end
+  end
+
+  describe '#is_within_range' do
+    describe '小数を引数に渡す' do
+      example '' do
+      end
+    end
+
+    describe '数字の文字列を引数に渡す' do
+      example '' do
+      end
+    end
+
+    describe '数字ではない文字列を引数に渡す' do
+      example '' do
+      end
+    end
+
+    describe '' do
+      example '' do
+      end
+    end
+
+    describe '' do
+      example '' do
+      end
     end
   end
 end
