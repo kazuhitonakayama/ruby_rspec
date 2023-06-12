bb9024cdaf0fe148b95971330df9e063a4831a2e
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 15:26:52 +0900

    :sparkles: 小数を引数に渡した時FloatNotAllowedErrorを返す実装を追加

--- lib/closed_range.rb
+++ lib/closed_range.rb
@@ -1,7 +1,11 @@
+class FloatNotAllowedError < StandardError
+end
+
 class ClosedRange
   def display_range_array(start_point, end_point)
     return [start_point] if start_point == end_point
     return StandardError if start_point > end_point
+    return FloatNotAllowedError unless start_point.is_a?(Integer) && end_point.is_a?(Integer)
 
     [start_point, end_point]
   end
