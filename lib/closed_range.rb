257de19a493894179588a1f1c8e26e2796c314bf
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 16:12:16 +0900

    :recycle: ClosedRangeにてinitializeを利用する

--- lib/closed_range.rb
+++ lib/closed_range.rb
@@ -2,11 +2,18 @@ class NotIntegerValueIsProvided < StandardError
 end
 
 class ClosedRange
-  def display_range_array(start_point, end_point)
-    return NotIntegerValueIsProvided unless start_point.is_a?(Integer) && end_point.is_a?(Integer)
-    return StandardError if start_point > end_point
+  attr_reader :start_point, :end_point
 
-    return [start_point] if start_point == end_point
-    [start_point, end_point]
+  def initialize(start_point, end_point)
+    @start_point = start_point
+    @end_point = end_point
+  end
+
+  def display_range_array
+    return NotIntegerValueIsProvided unless @start_point.is_a?(Integer) && @end_point.is_a?(Integer)
+    return StandardError if @start_point > @end_point
+
+    return [start_point] if @start_point == @end_point
+    [@start_point, @end_point]
   end
 end
--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -1,55 +1,48 @@
 describe ClosedRange do
-  let(:closed_range){ ClosedRange.new }
-
   describe '#display_range_array' do
     describe '整数の下端と上端（下端 < 上端）を引数に渡す' do
       example '下端として-1、上端として3を渡せば[-1, 3]を返す' do
-        expect(closed_range.display_range_array(-1, 3)).to eq [-1, 3]
+        expect(ClosedRange.new(-1, 3).display_range_array).to eq [-1, 3]
       end
     end
 
     describe '上端よりも下端が大きい' do
       example '下端として3、上端として1を渡せばStandardErrorを返す' do
-        expect(closed_range.display_range_array(3, 1)).to eq StandardError
+        expect(ClosedRange.new(3, 1).display_range_array).to eq StandardError
       end
     end
 
     describe '下端と上端が等しい' do
       example '下端として3、上端として3を渡せば[3]を返す' do
-        expect(closed_range.display_range_array(3, 3)).to eq [3]
+        expect(ClosedRange.new(3, 3).display_range_array).to eq [3]
       end
     end
 
-    describe '数字ではない値を引数に渡す' do
+    describe '整数ではない値を引数に渡す' do
       example '下端に"あ"、上端に"1"を渡せばNotIntegerValueIsProvided例外を返す' do
-        expect(closed_range.display_range_array('あ', '1')).to eq NotIntegerValueIsProvided
+        expect(ClosedRange.new('あ', '1').display_range_array).to eq NotIntegerValueIsProvided
       end
 
       example '下端として3.4、上端として4.4を渡せばNotIntegerValueIsProvided例外を返す' do
-        expect(closed_range.display_range_array(3.4, 4.4)).to eq NotIntegerValueIsProvided
+        expect(ClosedRange.new(3.4, 4.4).display_range_array).to eq NotIntegerValueIsProvided
       end
     end
   end
 
   describe '#is_within_range' do
-    describe '小数を引数に渡す' do
-      example ''
+    before do
+      closed_rang
     end
 
-    describe '数字の文字列を引数に渡す' do
-      example ''
+    describe '整数を引数に渡す' do
+      example '区間内にある時'
+      example '区間外にある時'
     end
 
-    describe '数字ではない文字列を引数に渡す' do
-      example ''
-    end
+    describe '整数ではない値を引数に渡す' do
+      example '小数を渡すとNotIntegerValueIsProvided例外を返す'
 
-    describe '' do
-      example ''
-    end
-
-    describe '' do
-      example ''
+      example '文字列を渡すとNotIntegerValueIsProvided例外を返す'
     end
   end
 end
