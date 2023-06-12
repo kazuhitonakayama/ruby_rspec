c7882e852bf6601d80b21078bdedc58922ab55a6
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 15:09:31 +0900

    :white_check_mark: 下端と上端が等しい時のテストを追加

--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -3,39 +3,40 @@ describe ClosedRange do
 
   describe '#display_range_array' do
     describe '整数の下端と上端（下端 < 上端）を引数に渡せば、下端と上端を値にもつArrayを返す' do
-      example '下端として1, 上端として3を渡せば[1, 3]を返す' do
+      example '下端として1、上端として3を渡せば[1, 3]を返す' do
         expect(closed_range.display_range_array(1, 3)).to eq [1, 3]
       end
     end
 
     describe '上端よりも下端が大きい' do
-      example '下端として3, 上端として1' do
+      example '下端として3、上端として1を渡せば' do
         expect(closed_range.display_range_array(3, 1)).to eq StandardError
       end
     end
 
     describe '下端と上端が等しい' do
-      example '' do
+      example '下端として3、上端として3を渡せば' do
+        expect(closed_range.display_range_array(3, 3)).to eq [3]
       end
     end
 
     describe '下端が負である' do
-      example '' do
+      example 'を渡せば' do
       end
     end
 
     describe '小数を引数に渡す' do
-      example '' do
+      example 'を渡せば' do
       end
     end
 
     describe '数字の文字列を引数に渡す' do
-      example '' do
+      example 'を渡せば' do
       end
     end
 
     describe '数字ではない文字列を引数に渡す' do
-      example '' do
+      example 'を渡せば' do
       end
     end
   end
