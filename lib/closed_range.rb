d750095ddb6bf78cc64b3b5f63e81cc12b06039c
kazu <kazuhito-nakayama@pepabo.com>
2023-06-12 15:46:45 +0900

    :recycle: 例外を吐く部分と正常処理のコードをわかりやすく

--- lib/closed_range.rb
+++ lib/closed_range.rb
@@ -7,9 +7,9 @@ end
 class ClosedRange
   def display_range_array(start_point, end_point)
     return NotIntegerValueIsProvided unless start_point.is_a?(Integer) && end_point.is_a?(Integer)
-    return [start_point] if start_point == end_point
     return StandardError if start_point > end_point
 
+    return [start_point] if start_point == end_point
     [start_point, end_point]
   end
 end
