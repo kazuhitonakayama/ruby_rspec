83bd25e33c9bbc484287143bf82c665fe7ef6565
kazu <kazuhito-nakayama@pepabo.com>
2023-06-14 17:25:49 +0900

    :recycle: letを用いて事前準備をする

--- spec/lib/sample_spec.rb
+++ spec/lib/sample_spec.rb
@@ -1,30 +1,34 @@
 describe ClosedRange do
   describe '#display_range_array' do
     context '整数の下端と上端（下端 < 上端）を引数に渡す' do
+      let(:closed_range) { ClosedRange.new(-1, 3) }
+
       example '下端として-1、上端として3を渡せば[-1,3]を返す' do
-        expect(ClosedRange.new(-1, 3).display_range_array).to eq [-1,3]
+        expect(closed_range.display_range_array).to eq [-1,3]
       end
     end
 
     context '上端よりも下端が大きい' do
+      let(:closed_range) { ClosedRange.new(3, 1) }
+
       example '下端として3、上端として1を渡せば InvertedRangeError を返す' do
-        expect{ ClosedRange.new(3, 1).display_range_array }.to raise_error(InvertedRangeError)
+        expect{ closed_range.display_range_array }.to raise_error(InvertedRangeError)
       end
     end
 
     context '下端と上端が等しい' do
+      let(:closed_range) { ClosedRange.new(3, 3) }
+
       example '下端として3、上端として3を渡せば[3]を返す' do
-        expect(ClosedRange.new(3, 3).display_range_array).to eq [3]
+        expect(closed_range.display_range_array).to eq [3]
       end
     end
 
     context '整数ではない値を引数に渡す' do
-      example '下端に"あ"、上端に"1"を渡せば NotIntegerValueIsProvided 例外を返す' do
-        expect{ ClosedRange.new('あ', '1').display_range_array }.to raise_error(NotIntegerValueIsProvided)
-      end
+      let(:closed_range) { ClosedRange.new(3.4, "あ") }
 
-      example '下端として3.4、上端として4.4を渡せば NotIntegerValueIsProvided 例外を返す' do
-        expect{ ClosedRange.new(3.4, 4.4).display_range_array }.to raise_error(NotIntegerValueIsProvided)
+      example '下端として3.4、上端として"あ"を渡せば NotIntegerValueIsProvided 例外を返す' do
+        expect{ closed_range.display_range_array }.to raise_error(NotIntegerValueIsProvided)
       end
     end
   end
